4dd643369cc8dac0a12ef925b0cf7ed765f5d555
Scala Steward <me@scala-steward.org>
2020-02-08 00:57:13 +0100

    Update cats-effect to 2.1.1

--- project/Settings.scala
+++ project/Settings.scala
@@ -7,7 +7,7 @@ object Settings {
   object LibraryVersions {
     val scalajsReact                = "1.6.0"
     val cats                        = "2.1.0"
-    val catsEffect                  = "2.1.0"
+    val catsEffect                  = "2.1.1"
     val fs2                         = "2.2.2"
     val monocle                     = "2.0.1"
     val log4Cats                    = "1.0.1"
