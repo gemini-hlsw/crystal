20bf1e6b278692ddabbd695d30dcdfcd922a767c
rpiaggio <rpiaggio@gmail.com>
2020-04-01 15:12:02 -0300

    StreamRendererMod Hold mechanism to avoid flicker

--- js/src/main/scala/crystal/react/StreamRendererMod.scala
+++ js/src/main/scala/crystal/react/StreamRendererMod.scala
@@ -3,7 +3,7 @@ package crystal.react
 import cats.effect._
 import cats.implicits._
 import japgolly.scalajs.react.component.Generic.UnmountedWithRoot
-import japgolly.scalajs.react._
+import japgolly.scalajs.react.{ Ref => ReactRef, _}
 import japgolly.scalajs.react.vdom.html_<^._
 import _root_.io.chrisdavenport.log4cats.Logger
 import _root_.io.chrisdavenport.log4cats.log4s.Log4sLogger
@@ -11,12 +11,59 @@ import _root_.io.chrisdavenport.log4cats.log4s.Log4sLogger
 import scala.scalajs.js
 
 import scala.language.higherKinds
+import cats.effect.concurrent.MVar
+import cats.effect.concurrent.Ref
+import scala.concurrent.duration.FiniteDuration
+import crystal.react.io.implicits._
 
 object StreamRendererMod {
   type ModState[A] = (A => A) => Callback
   type ReactStreamRendererProps[A] = (A, ModState[A]) => VdomNode
   type ReactStreamRendererComponent[A] = 
     CtorType.Props[ReactStreamRendererProps[A], UnmountedWithRoot[ReactStreamRendererProps[A], _, _, _]]
+
+  class Hold[A](
+    setter: A => IO[Unit],
+    duration: Option[FiniteDuration],
+    cancelToken: Ref[IO, Option[CancelToken[IO]]],
+    buffer: Ref[IO, Option[A]]
+  )(implicit cs: ContextShift[IO], timer: Timer[IO]) {
+    def set(a: A): IO[Unit] =
+      cancelToken.get.flatMap(
+        _.fold(setter(a))(_ => buffer.set(a.some))
+      )
+
+    private val restart: Option[IO[Unit]] =
+      duration.map{ d =>
+        for {
+          _ <- (cancelToken.getAndSet(None).flatMap(_.getOrElse(IO.unit))).uncancelable
+          _ <- Timer[IO].sleep(d)
+          _ <- cancelToken.set(None)
+          b <- buffer.getAndSet(None)
+          _ <- b.fold(IO.unit)(set)
+        } yield ()
+      }
+
+    val enable: IO[Unit] =
+      restart.fold(IO.unit){
+        _.runCancelable(_ => IO.unit).toIO.flatMap{ token =>
+          cancelToken.set(token.some)
+        }
+      }
+  }
+
+  object Hold {
+    def apply[A](
+      setter: A => IO[Unit],
+      duration: Option[FiniteDuration]
+    )(implicit cs: ContextShift[IO], timer: Timer[IO]): SyncIO[Hold[A]] =
+      for {
+        cancelToken <- Ref.in[SyncIO, IO, Option[CancelToken[IO]]](None)
+        buffer <- Ref.in[SyncIO, IO, Option[A]](None)
+      } yield {
+        new Hold(setter, duration, cancelToken, buffer)
+      }
+  }
 
   type State[A] = Option[A]
 
@@ -25,8 +72,9 @@ object StreamRendererMod {
   def build[F[_] : ConcurrentEffect, A](
       stream: fs2.Stream[F, A],
       reusability: Reusability[A] = Reusability.by_==[A],
-      key: js.UndefOr[js.Any] = js.undefined
-    ): ReactStreamRendererComponent[A] = {
+      key: js.UndefOr[js.Any] = js.undefined,
+      holdAfterMod: Option[FiniteDuration] = None
+    )(implicit cs: ContextShift[IO], timer: Timer[IO]): ReactStreamRendererComponent[A] = {
       implicit val propsReuse: Reusability[ReactStreamRendererProps[A]] = Reusability.byRef
       implicit val aReuse: Reusability[A] = reusability
 
@@ -34,10 +82,12 @@ object StreamRendererMod {
 
         var cancelToken: Option[CancelToken[F]] = None
 
+        val hold: Hold[Option[A]] = Hold(a => $.setStateIO(a), holdAfterMod).unsafeRunSync()
+
         val evalCancellable: SyncIO[CancelToken[F]] =
           ConcurrentEffect[F].runCancelable(
             stream
-              .evalMap(v => Sync[F].delay($.setState(Some(v)).runNow()))
+              .evalMap(v => Sync[F].delay(hold.set(v.some).runNow()))
               .compile.drain
           )(_.swap.toOption.foldMap(e => Logger[IO].error(e)("[StreamRendererMod] Error on stream")))
 
@@ -50,7 +100,7 @@ object StreamRendererMod {
         }
 
         def render(props: ReactStreamRendererProps[A], state: Option[A]): VdomNode = 
-          state.fold(VdomNode(null))(s => props(s, f => $.modState(_.map(f))))
+          state.fold(VdomNode(null))(s => props(s, f => hold.enable.flatMap(_ => $.modStateIO(_.map(f)))))
       }
 
       ScalaComponent
