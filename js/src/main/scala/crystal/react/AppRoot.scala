11322f33a93ea8df3d3d448f37ea4cd509d027c3
rpiaggio <rpiaggio@gmail.com>
2020-04-08 21:53:58 -0300

    Add onUnmount to AppRoot.

--- js/src/main/scala/crystal/react/AppRoot.scala
+++ js/src/main/scala/crystal/react/AppRoot.scala
@@ -1,11 +1,13 @@
 package crystal.react
 
+import cats.implicits._
 import cats.effect.ConcurrentEffect
 import cats.effect.Timer
 import japgolly.scalajs.react.component.Generic.UnmountedWithRoot
 import japgolly.scalajs.react._
 import japgolly.scalajs.react.vdom.html_<^._
 import crystal.ViewCtx
+import implicits._
 
 import scala.language.higherKinds
 import cats.kernel.Monoid
@@ -18,7 +20,16 @@ object AppRoot {
   def component[F[_]] = new Apply[F]
 
   class Apply[F[_]] {
-    def apply[M, C](model: M, ctx: C)(implicit ce: ConcurrentEffect[F], timer: Timer[F], monoidF: Monoid[F[Unit]]) = {
+    def apply[M, C](
+      model:       M,
+      ctx:         C
+    )(
+      onUnmount:   Option[F[Unit]] = None
+    )(
+      implicit ce: ConcurrentEffect[F],
+      timer:       Timer[F],
+      monoidF:     Monoid[F[Unit]]
+    ) = {
       // TODO We don't really need the whole Stream machinery for this. Make a simpler component
       // that just receives an initial state.
       val Renderer = crystal.react.StreamRendererMod.build[F, M](fs2.Stream.emit(model).covary[F])
@@ -27,6 +38,7 @@ object AppRoot {
         .render_P{ props =>
           Renderer(m => props(ViewCtx(m, ctx)))
         }
+        .componentWillUnmount(_ => onUnmount.orEmpty.toCB)
         .build
     }    
   }
