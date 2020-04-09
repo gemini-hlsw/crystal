558b5afd65dd840db4eba7fbd8a207b0622b674c
rpiaggio <rpiaggio@gmail.com>
2020-04-08 21:21:00 -0300

    Fix tagless hold mechanism.

--- js/src/main/scala/crystal/react/AppRoot.scala
+++ js/src/main/scala/crystal/react/AppRoot.scala
@@ -8,6 +8,7 @@ import japgolly.scalajs.react.vdom.html_<^._
 import crystal.ViewCtx
 
 import scala.language.higherKinds
+import cats.kernel.Monoid
 
 object AppRoot {
   type AppRootProps[F[_], C, M] = ViewCtx[F, C, M] => VdomNode
@@ -17,7 +18,7 @@ object AppRoot {
   def component[F[_]] = new Apply[F]
 
   class Apply[F[_]] {
-    def apply[M, C](model: M, ctx: C)(implicit ce: ConcurrentEffect[F], timer: Timer[F]) = {
+    def apply[M, C](model: M, ctx: C)(implicit ce: ConcurrentEffect[F], timer: Timer[F], monoidF: Monoid[F[Unit]]) = {
       // TODO We don't really need the whole Stream machinery for this. Make a simpler component
       // that just receives an initial state.
       val Renderer = crystal.react.StreamRendererMod.build[F, M](fs2.Stream.emit(model).covary[F])
--- js/src/main/scala/crystal/react/StreamRendererMod.scala
+++ js/src/main/scala/crystal/react/StreamRendererMod.scala
@@ -17,8 +17,8 @@ import cats.effect.concurrent.Ref
 import scala.concurrent.duration.FiniteDuration
 import crystal.react.implicits._
 import japgolly.scalajs.react.extra.StateSnapshot
-import cats.Applicative
 import crystal.View
+import cats.kernel.Monoid
 
 object StreamRendererMod {
   type ReactStreamRendererProps[F[_], A] = View[F, A] => VdomNode
@@ -30,6 +30,8 @@ object StreamRendererMod {
     duration: Option[FiniteDuration],
     cancelToken: Ref[F, Option[CancelToken[F]]],
     buffer: Ref[F, Option[A]]
+  )(
+    implicit monoidF: Monoid[F[Unit]]
   ) {
     def set(a: A): F[Unit] =
       cancelToken.get.flatMap(
@@ -39,26 +41,28 @@ object StreamRendererMod {
     private val restart: Option[F[Unit]] =
       duration.map{ d =>
         for {
-          _ <- (cancelToken.getAndSet(None).flatMap(_.getOrElse(Applicative[F].unit))).uncancelable
+          _ <- (cancelToken.getAndSet(None).flatMap(_.orEmpty)).uncancelable
           _ <- Timer[F].sleep(d)
           _ <- cancelToken.set(None)
           b <- buffer.getAndSet(None)
-          _ <- b.fold(Applicative[F].unit)(set)
+          _ <- b.map(set).orEmpty
         } yield ()
       }
 
     val enable: F[Unit] =
-      restart.fold(Applicative[F].unit){ r =>
-        Applicative[F].pure(r.runCancelable(_ => IO.unit).unsafeRunSync()).flatMap{ token => // TODO Error handling
+      restart.map{ r =>
+        Sync[F].delay(r.runCancelable(_ => IO.unit).unsafeRunSync()).flatMap{ token => // TODO Error handling
           cancelToken.set(token.some)
         }
-      }
+      }.orEmpty
   }
 
   object Hold {
     def apply[F[_] : ConcurrentEffect : Timer, A](
       setter: A => F[Unit],
       duration: Option[FiniteDuration]
+    )(
+      implicit monoidF: Monoid[F[Unit]]
     ): SyncIO[Hold[F, A]] =
       for {
         cancelToken <- Ref.in[SyncIO, F, Option[CancelToken[F]]](None)
@@ -77,6 +81,8 @@ object StreamRendererMod {
       reusability: Reusability[A] = Reusability.by_==[A],
       key: js.UndefOr[js.Any] = js.undefined,
       holdAfterMod: Option[FiniteDuration] = None
+    )(
+      implicit monoidF: Monoid[F[Unit]]
     ): ReactStreamRendererComponent[F, A] = {
       implicit val propsReuse: Reusability[ReactStreamRendererProps[F, A]] = Reusability.byRef
       implicit val aReuse: Reusability[A] = reusability
@@ -102,22 +108,11 @@ object StreamRendererMod {
           cancelToken.map(_.startAsCallbackAndForget()).getOrEmpty
 
         def render(props: ReactStreamRendererProps[F, A], state: Option[A]): VdomNode = 
-          state.fold(VdomNode(null))(s => props(
+          state.fold(VdomNode(null))(s =>
+            props(
               View[F, A](s, f => hold.enable.flatMap(_ => $.modStateIn[F]((v: Option[A]) => v.map(f))))
-/*
-
-            StateSnapshot(s){ // TODO It would be nice to have a generic StateSnapshot[F]
-              (newState: Option[A], cb: Callback) => 
-              
-                println(s"IN SNAPSHOT SETTER: [$newState]")
-              
-                hold.enable.flatMap(_ => $.setStateIn[F](newState)).startAsCallbackAndThen(cb)
-            }
-
-          }*/
-
-
-          ))
+            )
+          )
       }
 
       ScalaComponent
