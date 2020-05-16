3a9326268bf23ca7ee0fe763b38602cab9b66ea3
rpiaggio <rpiaggio@gmail.com>
2020-05-15 23:45:36 -0300

    Pot monad.

---
+++ shared/src/main/scala/crystal/data/Pot.scala
@@ -0,0 +1,87 @@
+package crystal.data
+
+import cats.Monad
+import scala.annotation.tailrec
+import cats.kernel.Eq
+import cats.implicits._
+
+sealed trait Pot[A] {
+  def map[B](f: A => B): Pot[B] =
+    this match {
+      case Pot.Pending(start) => Pot.Pending(start)
+      case Pot.Error(t)       => Pot.Error(t)
+      case Pot.Ready(a)       => Pot.Ready(f(a))
+    }
+
+  def fold[B](fp: Long => B, fe: Throwable => B, fr: A => B): B =
+    this match {
+      case Pot.Pending(start) => fp(start)
+      case Pot.Error(t)       => fe(t)
+      case Pot.Ready(a)       => fr(a)
+    }
+
+  def flatten[B](implicit ev: A <:< Pot[B]): Pot[B] =
+    this match {
+      case Pot.Pending(start) => Pot.Pending(start)
+      case Pot.Error(t)       => Pot.Error(t)
+      case Pot.Ready(a)       => a
+    }
+
+  def flatMap[B](f: A => Pot[B]): Pot[B] =
+    map(f).flatten
+
+  def toOption: Option[A] =
+    this match {
+      case Pot.Ready(a) => a.some
+      case _            => none
+    }
+}
+
+object Pot {
+  final case class Pending[A](start: Long = System.currentTimeMillis()) extends Pot[A]
+  final case class Error[A](t: Throwable) extends Pot[A]
+  final case class Ready[A](value: A) extends Pot[A]
+
+  def apply[A](a: A): Pot[A] = Ready(a)
+
+  private implicit val eqT: Eq[Throwable] = Eq.by(_.getClass.getName)
+
+  implicit def potEq[A: Eq]: Eq[Pot[A]] =
+    new Eq[Pot[A]] {
+      def eqv(x: Pot[A], y: Pot[A]): Boolean =
+        x match {
+          case Pending(startx) =>
+            y match {
+              case Pending(starty) => startx === starty
+              case _               => false
+            }
+          case Error(tx)       =>
+            y match {
+              case Error(ty) => tx === ty
+              case _         => false
+            }
+          case Ready(ax)       =>
+            y match {
+              case Ready(ay) => ax === ay
+              case _         => false
+            }
+        }
+    }
+
+  implicit object PotMonad extends Monad[Pot] {
+    override def pure[A](a: A): Pot[A] = Pot(a)
+
+    @tailrec
+    override def tailRecM[A, B](a: A)(f: A => Pot[Either[A, B]]): Pot[B] =
+      f(a) match {
+        case Pending(start)  => Pending(start)
+        case Error(t)        => Error(t)
+        case Ready(Left(a))  => tailRecM(a)(f)
+        case Ready(Right(b)) => Ready(b)
+      }
+
+    override def flatMap[A, B](fa: Pot[A])(f: A => Pot[B]): Pot[B] =
+      fa.flatMap(f)
+  }
+
+}
---
+++ shared/src/test/scala/crystal/data/PotSpec.scala
@@ -0,0 +1,33 @@
+package crystal.data
+
+import munit.DisciplineSuite
+import cats.laws.discipline.MonadTests
+import arbitraries._
+import cats.implicits._
+import cats.kernel.laws.discipline.EqTests
+import org.scalacheck.Prop.forAll
+
+class PotSpec extends DisciplineSuite {
+  checkAll(
+    "Pot.EqLaws",
+    EqTests[Pot[Int]].eqv
+  )
+
+  checkAll(
+    "Pot.MonadLaws",
+    MonadTests[Pot].monad[Int, Int, String]
+  )
+
+  property("Pot.toOption: Pending(_) === None") {
+    forAll((l: Long) => Pot.Pending[Int](l).toOption === none)
+
+  }
+
+  property("Pot.toOption: Error(_) === None") {
+    forAll((t: Throwable) => Pot.Error[Int](t).toOption === none)
+  }
+
+  property("Pot.toOption: Ready(a) === Some(a)") {
+    forAll((i: Int) => Pot.Ready(i).toOption === i.some)
+  }
+}
