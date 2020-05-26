a516737f5ec86922f8ac91e08610f2b534bb727c
rpiaggio <rpiaggio@gmail.com>
2020-05-26 13:55:44 -0300

    Rename View->ViewF, allowing View alias.

--- shared/src/main/scala/crystal/ActionInterpreter.scala
+++ shared/src/main/scala/crystal/ActionInterpreter.scala
@@ -1,9 +1,9 @@
 package crystal
 
 trait ActionInterpreterOpt[F[_], A[_[_]], T] {
-  def of(view: ViewOpt[F, T]): A[F]
+  def of(view: ViewOptF[F, T]): A[F]
 }
 
 trait ActionInterpreter[F[_], A[_[_]], T] {
-  def of(view: View[F, T]): A[F]
+  def of(view: ViewF[F, T]): A[F]
 }
--- shared/src/main/scala/crystal/ViewF.scala
+++ shared/src/main/scala/crystal/ViewF.scala
@@ -5,9 +5,9 @@ import monocle.Lens
 import monocle.Optional
 import monocle.Prism
 
-class ViewOpt[F[_], A](
-    value: Option[A],
-    val modOpt: (Option[A] => Option[A]) => F[Unit]
+class ViewOptF[F[_], A](
+  value:      Option[A],
+  val modOpt: (Option[A] => Option[A]) => F[Unit]
 ) {
   @inline def getOption: Option[A] = value
 
@@ -17,65 +17,65 @@ class ViewOpt[F[_], A](
 
   @inline def setOpt(a: Option[A]): F[Unit] = modOpt(_ => a)
 
-  def zoom[B](get: A => B)(modB: (B => B) => A => A): ViewOpt[F, B] =
-    new ViewOpt(
+  def zoom[B](get: A => B)(modB: (B => B) => A => A): ViewOptF[F, B] =
+    new ViewOptF(
       value.map(get),
       (f: Option[B] => Option[B]) => mod(modB(b => f(b.some).getOrElse(b)))
     )
 
   def zoomOpt[B](
-      get: A => Option[B]
-  )(modB: (B => B) => A => A): ViewOpt[F, B] =
-    new ViewOpt(
+    get:  A => Option[B]
+  )(modB: (B => B) => A => A): ViewOptF[F, B] =
+    new ViewOptF(
       value.flatMap(get),
       (f: Option[B] => Option[B]) => mod(modB(b => f(b.some).getOrElse(b)))
     )
 
   def zoomOptOpt[B](
-      get: A => Option[B]
-  )(modB: (Option[B] => Option[B]) => Option[A] => Option[A]): ViewOpt[F, B] =
-    new ViewOpt(
+    get:  A => Option[B]
+  )(modB: (Option[B] => Option[B]) => Option[A] => Option[A]): ViewOptF[F, B] =
+    new ViewOptF(
       value.flatMap(get),
       (f: Option[B] => Option[B]) => modOpt(modB(f))
     )
 
-  def zoomL[B](lens: Lens[A, B]): ViewOpt[F, B] =
+  def zoomL[B](lens: Lens[A, B]): ViewOptF[F, B] =
     zoom(lens.get)(lens.modify)
 
-  def zoomO[B](optional: Optional[A, B]): ViewOpt[F, B] =
+  def zoomO[B](optional: Optional[A, B]): ViewOptF[F, B] =
     zoomOpt(optional.getOption)(optional.modify)
 
-  def zoomP[B](prism: Prism[A, B]): ViewOpt[F, B] =
+  def zoomP[B](prism: Prism[A, B]): ViewOptF[F, B] =
     zoomOpt(prism.getOption)(prism.modify)
 }
 
-object ViewOpt {
+object ViewOptF {
   def apply[F[_], A](
-      value: Option[A],
-      modOpt: (Option[A] => Option[A]) => F[Unit]
-  ): ViewOpt[F, A] =
-    new ViewOpt(value, modOpt)
+    value:  Option[A],
+    modOpt: (Option[A] => Option[A]) => F[Unit]
+  ): ViewOptF[F, A] =
+    new ViewOptF(value, modOpt)
 }
 
 // The difference between a View and a StateSnapshot is that the modifier doesn't act on the current value,
 // but passes the modifier function to an external source of truth. Since we are defining no getter
 // from such source of truth, a View is defined in terms of a modifier function instead of a setter.
-class View[F[_], A](value: A, mod: (A => A) => F[Unit])
-    extends ViewOpt[F, A](
+class ViewF[F[_], A](value: A, mod: (A => A) => F[Unit])
+    extends ViewOptF[F, A](
       value.some,
       (f: Option[A] => Option[A]) => mod(a => f(a.some).getOrElse(a))
       // (funcOptA => funcOptA((a: Option[A]) => a.map(mod))  )
     ) {
   @inline def get: A = value
 
-  override def zoom[B](get: A => B)(modB: (B => B) => A => A): View[F, B] =
-    new View(get(value), (f: B => B) => mod(modB(f)))
+  override def zoom[B](get:   A => B)(modB: (B => B) => A => A): ViewF[F, B] =
+    new ViewF(get(value), (f: B => B) => mod(modB(f)))
 
-  override def zoomL[B](lens: Lens[A, B]): View[F, B] =
+  override def zoomL[B](lens: Lens[A, B]): ViewF[F, B] =
     zoom(lens.get)(lens.modify)
 }
 
-object View {
-  def apply[F[_], A](value: A, mod: (A => A) => F[Unit]): View[F, A] =
-    new View(value, mod)
+object ViewF {
+  def apply[F[_], A](value: A, mod: (A => A) => F[Unit]): ViewF[F, A] =
+    new ViewF(value, mod)
 }
