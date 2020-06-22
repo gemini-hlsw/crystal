a3db4e47091d66ff7c942c98be16c6100fb4821f
rpiaggio <rpiaggio@gmail.com>
2020-06-22 19:28:59 -0300

    New ScalaFmt.

--- jvm/src/main/scala/crystal/ComponentTypesForPlatform.scala
+++ jvm/src/main/scala/crystal/ComponentTypesForPlatform.scala
@@ -1,7 +1,7 @@
 package crystal
 
 trait ComponentTypesForPlatform extends ComponentTypes {
-  type StreamRenderer[A] = Nothing
+  type StreamRenderer[A]          = Nothing
   type StreamRendererMod[F[_], A] = Nothing
-  type AppRoot[M] = Nothing
+  type AppRoot[M]                 = Nothing
 }
