f6eb7dbdde73f743dabf29dabf529a6654aee362
Scala Steward <me@scala-steward.org>
2020-11-04 17:00:25 +0100

    Update discipline-core to 1.1.0

--- project/Settings.scala
+++ project/Settings.scala
@@ -12,7 +12,7 @@ object Settings {
     val monocle         = "2.1.0"
     val log4Cats        = "1.1.1"
     val mUnit           = "0.7.16"
-    val discipline      = "1.0.3"
+    val discipline      = "1.1.0"
     val disciplineMUnit = "0.3.0"
   }
 
