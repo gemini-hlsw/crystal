2b1e6654f87cf4003ce571daf1b080263bbfe5dd
Scala Steward <me@scala-steward.org>
2021-05-14 02:58:00 +0200

    Update discipline-core to 1.1.5

--- project/Settings.scala
+++ project/Settings.scala
@@ -13,7 +13,7 @@ object Settings {
     val log4Cats        = "2.1.0"
     val mUnit           = "0.7.26"
     val mUnitCatsEffect = "1.0.2"
-    val discipline      = "1.1.4"
+    val discipline      = "1.1.5"
     val disciplineMUnit = "1.0.9"
   }
 
