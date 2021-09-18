1b54ad66cf5e62b68ab589804ed97daf9bb8e229
Scala Steward <me@scala-steward.org>
2021-09-18 08:06:36 +0200

    Update cats-effect to 3.2.9

--- project/Settings.scala
+++ project/Settings.scala
@@ -7,7 +7,7 @@ object Settings {
   object LibraryVersions {
     val scalajsReact    = "2.0.0-RC3"
     val cats            = "2.6.1"
-    val catsEffect      = "3.2.8"
+    val catsEffect      = "3.2.9"
     val fs2             = "3.1.2"
     val monocle         = "3.1.0"
     val log4Cats        = "2.1.1"
