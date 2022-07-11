9bedb6f56dcc4e6ad9cf8ac00d95b3051d823e49
Raúl Piaggio <rpiaggio@gemini.edu>
2022-07-11 18:58:57 -0300

    Minor syntax improvements.

--- js/src/main/scala/crystal/react/hooks/UseEffectResult.scala
+++ js/src/main/scala/crystal/react/hooks/UseEffectResult.scala
@@ -8,13 +8,13 @@ import japgolly.scalajs.react.hooks.CustomHook
 import japgolly.scalajs.react.util.DefaultEffects.{ Async => DefaultA }
 
 object UseEffectResult {
-  def hook[D: Reusability, A] = CustomHook[(D, D => DefaultA[A])]
+  def hook[D: Reusability, A] = CustomHook[WithDeps[D, DefaultA[A]]]
     .useState(Pot.pending[A])
-    .useEffectWithDepsBy((props, _) => props._1)((_, state) => _ => state.setState(Pot.pending))
-    .useEffectWithDepsBy((props, _) => props._1)((props, state) =>
+    .useEffectWithDepsBy((props, _) => props.deps)((_, state) => _ => state.setState(Pot.pending))
+    .useEffectWithDepsBy((props, _) => props.deps)((props, state) =>
       deps =>
         (for {
-          a <- props._2(deps)
+          a <- props.fromDeps(deps)
           _ <- state.setStateAsync(a.ready)
         } yield ()).handleErrorWith(t => state.setStateAsync(Pot.error(t)))
     )
@@ -23,7 +23,7 @@ object UseEffectResult {
   object HooksApiExt {
     sealed class Primary[Ctx, Step <: HooksApi.AbstractStep](api: HooksApi.Primary[Ctx, Step]) {
 
-      /** Runs an async effect and stores the result in a state, which is provide as a `Pot[A]`.
+      /** Runs an async effect and stores the result in a state, which is provided as a `Pot[A]`.
         */
       final def useEffectResultWithDeps[D: Reusability, A](
         deps:   => D
@@ -32,21 +32,21 @@ object UseEffectResult {
       ): step.Next[Pot[A]] =
         useEffectResultWithDepsBy(_ => deps)(_ => effect)
 
-      /** Runs an async effect and stores the result in a state, which is provide as a `Pot[A]`.
+      /** Runs an async effect and stores the result in a state, which is provided as a `Pot[A]`.
         */
       final def useEffectResult[A](effect: DefaultA[A])(implicit
         step:                              Step
       ): step.Next[Pot[A]] =
         useEffectResultBy(_ => effect)
 
-      /** Runs an async effect and stores the result in a state, which is provide as a `Pot[A]`.
+      /** Runs an async effect and stores the result in a state, which is provided as a `Pot[A]`.
         */
       final def useEffectResultOnMount[A](effect: DefaultA[A])(implicit
         step:                                     Step
       ): step.Next[Pot[A]] =
         useEffectResultOnMountBy(_ => effect)
 
-      /** Runs an async effect and stores the result in a state, which is provide as a `Pot[A]`.
+      /** Runs an async effect and stores the result in a state, which is provided as a `Pot[A]`.
         */
       final def useEffectResultWithDepsBy[D: Reusability, A](
         deps:   Ctx => D
@@ -55,17 +55,17 @@ object UseEffectResult {
       ): step.Next[Pot[A]] =
         api.customBy { ctx =>
           val hookInstance = hook[D, A]
-          hookInstance((deps(ctx), effect(ctx)))
+          hookInstance(WithDeps(deps(ctx), effect(ctx)))
         }
 
-      /** Runs an async effect and stores the result in a state, which is provide as a `Pot[A]`.
+      /** Runs an async effect and stores the result in a state, which is provided as a `Pot[A]`.
         */
       final def useEffectResultBy[A](effect: Ctx => DefaultA[A])(implicit
         step:                                Step
       ): step.Next[Pot[A]] =
         useEffectResultWithDepsBy(_ => NeverReuse)(ctx => _ => effect(ctx))
 
-      /** Runs an async effect and stores the result in a state, which is provide as a `Pot[A]`.
+      /** Runs an async effect and stores the result in a state, which is provided as a `Pot[A]`.
         */
       final def useEffectResultOnMountBy[A](effect: Ctx => DefaultA[A])(implicit
         step:                                       Step
@@ -77,7 +77,7 @@ object UseEffectResult {
       api: HooksApi.Secondary[Ctx, CtxFn, Step]
     ) extends Primary[Ctx, Step](api) {
 
-      /** Runs an async effect and stores the result in a state, which is provide as a `Pot[A]`.
+      /** Runs an async effect and stores the result in a state, which is provided as a `Pot[A]`.
         */
       def useEffectResultWithDepsBy[D: Reusability, A](
         deps:   CtxFn[D]
@@ -86,14 +86,14 @@ object UseEffectResult {
       ): step.Next[Pot[A]] =
         useEffectResultWithDepsBy(step.squash(deps)(_))(step.squash(effect)(_))
 
-      /** Runs an async effect and stores the result in a state, which is provide as a `Pot[A]`.
+      /** Runs an async effect and stores the result in a state, which is provided as a `Pot[A]`.
         */
       final def useEffectResult[A](effect: CtxFn[DefaultA[A]])(implicit
         step:                              Step
       ): step.Next[Pot[A]] =
         useEffectResultBy(step.squash(effect)(_))
 
-      /** Runs an async effect and stores the result in a state, which is provide as a `Pot[A]`.
+      /** Runs an async effect and stores the result in a state, which is provided as a `Pot[A]`.
         */
       final def useEffectResultOnMountBy[A](effect: CtxFn[DefaultA[A]])(implicit
         step:                                       Step
