424be072850b89c6bc182e5bc5e8d0af35b28485
Raúl Piaggio <rpiaggio@gemini.edu>
2023-10-11 12:57:18 -0300

    useEffectWhenDepsReady

--- js/src/main/scala/crystal/react/hooks/package.scala
+++ js/src/main/scala/crystal/react/hooks/package.scala
@@ -5,6 +5,7 @@ package crystal.react.hooks
 
 import cats.effect.Fiber
 import cats.effect.Resource
+import crystal.Pot
 import crystal.react.reuse.*
 import japgolly.scalajs.react.*
 import japgolly.scalajs.react.util.DefaultEffects.{Async => DefaultA}
@@ -12,7 +13,7 @@ import japgolly.scalajs.react.util.DefaultEffects.{Async => DefaultA}
 export UseSingleEffect.syntax.*, UseSerialState.syntax.*, UseStateCallback.syntax.*,
   UseStateView.syntax.*, UseStateViewWithReuse.syntax.*, UseSerialStateView.syntax.*,
   UseAsyncEffect.syntax.*, UseEffectResult.syntax.*, UseResource.syntax.*,
-  UseStreamResource.syntax.*
+  UseStreamResource.syntax.*, UseEffectWhenDepsReady.syntax.*
 
 type UnitFiber[F[_]] = Fiber[F, Throwable, Unit]
 type AsyncUnitFiber  = Fiber[DefaultA, Throwable, Unit]
@@ -23,3 +24,5 @@ protected[hooks] type NeverReuse = Reuse[Unit]
 protected[hooks] val NeverReuse: NeverReuse = ().reuseNever
 
 protected[hooks] final case class WithDeps[D, A](deps: D, fromDeps: D => A)
+
+protected[hooks] final case class WithPotDeps[D, A](deps: Pot[D], fromDeps: D => A)
