b4518f9cef96bbde43d174fb23d6b2bdf9ea9ed4
Raúl Piaggio <rpiaggio@gemini.edu>
2024-12-28 17:54:33 -0300

    working

--- modules/tests/js/src/test/scala/crystal/react/hooks/UseSignalStreamSpec.scala
+++ modules/tests/js/src/test/scala/crystal/react/hooks/UseSignalStreamSpec.scala
@@ -3,7 +3,6 @@
 
 package crystal.react.hooks
 
-// import munit.FunSuite
 import munit.CatsEffectSuite
 import japgolly.scalajs.react.*
 import japgolly.scalajs.react.vdom.html_<^.*
@@ -13,28 +12,27 @@ import org.scalajs.dom
 import japgolly.scalajs.react.test.Simulate
 import cats.effect.IO
 import scala.concurrent.duration.*
+import cats.effect.kernel.Deferred
 
 class UseSignalStreamSuite extends CatsEffectSuite:
 
-  // test("SEE If this runs"):
-  //   IO.println("*******RUNSSSSS*********") >>
-  //     IO(assertEquals(10, 11)) >>
-  //     assertIO(IO(42), 43)
-
   test("useSignalStreamSuite"):
-    var result: List[Int] = List.empty
+    // Completes when the stream ready and we then start the test.
+    val readyLatch: Deferred[IO, Unit]  = Deferred.unsafe[IO, Unit]
+    // Completes when the stream terminates
+    val result: Deferred[IO, List[Int]] = Deferred.unsafe[IO, List[Int]]
 
     def consume(stream: fs2.Stream[IO, Int]): IO[Unit] =
-      stream.compile.toList.map:
-        result = _
+      stream.compile.toList
+        .flatMap(l => result.complete(l))
+        .void
 
     val inner = ScalaFnComponent[Int]: props =>
       for
         stream <- useSignalStream(props)
-        _      <- useEffectWhenDepsReady(stream)(s => consume(s.evalTap(IO.println(_))))
-      yield
-      // println(s"props: $props")
-      EmptyVdom
+        _      <- useEffectWhenDepsReady(stream): s =>
+                    readyLatch.complete(()) >> consume(s)
+      yield EmptyVdom
 
     val buttonRef = Ref[dom.HTMLButtonElement]
 
@@ -48,10 +46,9 @@ class UseSignalStreamSuite extends CatsEffectSuite:
     ReactTestUtils2
       .withRendered(outer())
       .async: d =>
-        IO.sleep(100.millis) >> IO.println(" FDEFEWFWE") >> IO {
-          Simulate.click(buttonRef.unsafeGet())
-          d.unmount()
-          assertEquals(result, List(0, 1))
-        }
-    // .onError(IO.println(_))
-    // .unsafeRunAndForget()
+        readyLatch.get >>
+          IO {
+            Simulate.click(buttonRef.unsafeGet())
+            d.unmount()
+          } >>
+          result.get.map(r => assertEquals(_, List(0, 1)))
