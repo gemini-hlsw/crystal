a92fa8f021eb6ed8cb85295a718729462465efd9
Raúl Piaggio <rpiaggio@gemini.edu>
2025-02-10 19:13:58 -0300

    tests, docs

--- modules/tests/js/src/test/scala/crystal/react/hooks/UseEffectResultSpec.scala
+++ modules/tests/js/src/test/scala/crystal/react/hooks/UseEffectResultSpec.scala
@@ -4,14 +4,17 @@
 package crystal.react.hooks
 
 import cats.effect.IO
+import cats.effect.std.Queue
 import cats.syntax.all.*
+import crystal.Pot
+import crystal.syntax.*
 import japgolly.scalajs.react.*
 import japgolly.scalajs.react.test.ReactTestUtils2
 import japgolly.scalajs.react.test.Simulate
 import japgolly.scalajs.react.vdom.html_<^.*
 import munit.CatsEffectSuite
 import org.scalajs.dom
-import cats.effect.std.Queue
+
 import scala.concurrent.duration.*
 
 class UseEffectResultSpec extends CatsEffectSuite:
@@ -118,6 +121,82 @@ class UseEffectResultSpec extends CatsEffectSuite:
              yield ()
     yield ()
 
+  test("useEffectResultWhenDepsReady - call every time deps become ready"):
+    val buttonRef = Ref[dom.HTMLButtonElement]
+
+    val comp = ScalaFnComponent[Queue[IO, Int]]: q =>
+      for
+        s <- useState(Pot.pending[Int])
+        v <- useEffectResultWhenDepsReady(s.value): _ =>
+               IO.sleep(50.millis) >> q.take
+      yield <.button(
+        (v.value, v.isRunning).toString,
+        ^.onClick --> s.modState(_.fold(0, _ => 0, _ + 1).ready),
+        ^.onDoubleClick --> s.setState(Pot.error(RuntimeException("error")))
+      ).withRef(buttonRef)
+
+    for
+      q <- buildQueue
+      _ <- withRendered(comp(q)): d =>
+             d.innerHTML.assert("(Pending,false)")
+             for
+               _ <- act(IO.sleep(50.millis))
+               _  = d.innerHTML.assert("(Pending,false)")             // Still pending
+               _ <- act_(Simulate.click(buttonRef.unsafeGet()))       // Change to ready
+               _  = d.innerHTML.assert("(Pending,true)")
+               _ <- act(IO.sleep(50.millis))
+               _  = d.innerHTML.assert("(Ready(1),false)")
+               _ <- act_(Simulate.click(buttonRef.unsafeGet()))       // Increment, don't change value
+               _  = d.innerHTML.assert("(Ready(1),false)")
+               _ <- act_(Simulate.doubleClick(buttonRef.unsafeGet())) // Change to error
+               _  = d.innerHTML.assert("(Ready(1),false)")            // No change in value
+               _ <- act_(Simulate.click(buttonRef.unsafeGet()))       // Change to ready again
+               _  = d.innerHTML.assert("(Pending,true)")
+               _ <- act(IO.sleep(50.millis))
+               _  = d.innerHTML.assert("(Ready(2),false)")
+             yield ()
+    yield ()
+
+  test("useEffectResultWhenDepsReadyOrChange - call every time deps become ready or change"):
+    val buttonRef = Ref[dom.HTMLButtonElement]
+
+    val comp = ScalaFnComponent[Queue[IO, Int]]: q =>
+      for
+        s <- useState(Pot.pending[MyInt])
+        v <- useEffectResultWhenDepsReadyOrChange(s.value): _ =>
+               IO.sleep(50.millis) >> q.take
+      yield <.button(
+        (v.value, v.isRunning).toString,
+        ^.onClick --> s.modState(_.fold(MyInt(0), _ => MyInt(0), _.inc).ready),
+        ^.onDoubleClick --> s.setState(Pot.error(RuntimeException("error")))
+      ).withRef(buttonRef)
+
+    for
+      q <- buildQueue
+      _ <- withRendered(comp(q)): d =>
+             d.innerHTML.assert("(Pending,false)")
+             for
+               _ <- act(IO.sleep(50.millis))
+               _  = d.innerHTML.assert("(Pending,false)")             // Still pending
+               _ <- act_(Simulate.click(buttonRef.unsafeGet()))       // Change to ready
+               _  = d.innerHTML.assert("(Pending,true)")
+               _ <- act(IO.sleep(50.millis))
+               _  = d.innerHTML.assert("(Ready(1),false)")
+               _ <- act_(Simulate.click(buttonRef.unsafeGet()))       // Increment, don't change value
+               _  = d.innerHTML.assert("(Ready(1),false)")
+               _ <- act_(Simulate.click(buttonRef.unsafeGet()))       // Increment again, should change
+               _  = d.innerHTML.assert("(Pending,true)")
+               _ <- act(IO.sleep(50.millis))
+               _  = d.innerHTML.assert("(Ready(2),false)")
+               _ <- act_(Simulate.doubleClick(buttonRef.unsafeGet())) // Change to error
+               _  = d.innerHTML.assert("(Ready(2),false)")            // No change in value
+               _ <- act_(Simulate.click(buttonRef.unsafeGet()))       // Change to ready again
+               _  = d.innerHTML.assert("(Pending,true)")
+               _ <- act(IO.sleep(50.millis))
+               _  = d.innerHTML.assert("(Ready(3),false)")
+             yield ()
+    yield ()
+
   test("useEffectKeepResultOnMount - only call once"):
     val buttonRef = Ref[dom.HTMLButtonElement]
 
@@ -206,7 +285,78 @@ class UseEffectResultSpec extends CatsEffectSuite:
              yield ()
     yield ()
 
-// useEffectKeepResultWhenDepsReady,
-// useEffectKeepResultWhenDepsReadyOrChange,
-// useEffectResultWhenDepsReady,
-// useEffectResultWhenDepsReadyOrChange,
+  test("useEffectKeepResultWhenDepsReady - call every time deps become ready"):
+    val buttonRef = Ref[dom.HTMLButtonElement]
+
+    val comp = ScalaFnComponent[Queue[IO, Int]]: q =>
+      for
+        s <- useState(Pot.pending[Int])
+        v <- useEffectKeepResultWhenDepsReady(s.value): _ =>
+               IO.sleep(50.millis) >> q.take
+      yield <.button(
+        (v.value, v.isRunning).toString,
+        ^.onClick --> s.modState(_.fold(0, _ => 0, _ + 1).ready),
+        ^.onDoubleClick --> s.setState(Pot.error(RuntimeException("error")))
+      ).withRef(buttonRef)
+
+    for
+      q <- buildQueue
+      _ <- withRendered(comp(q)): d =>
+             d.innerHTML.assert("(Pending,false)")
+             for
+               _ <- act(IO.sleep(50.millis))
+               _  = d.innerHTML.assert("(Pending,false)")             // Still pending
+               _ <- act_(Simulate.click(buttonRef.unsafeGet()))       // Change to ready
+               _  = d.innerHTML.assert("(Pending,true)")
+               _ <- act(IO.sleep(50.millis))
+               _  = d.innerHTML.assert("(Ready(1),false)")
+               _ <- act_(Simulate.click(buttonRef.unsafeGet()))       // Increment, don't change value
+               _  = d.innerHTML.assert("(Ready(1),false)")
+               _ <- act_(Simulate.doubleClick(buttonRef.unsafeGet())) // Change to error
+               _  = d.innerHTML.assert("(Ready(1),false)")            // No change in value
+               _ <- act_(Simulate.click(buttonRef.unsafeGet()))       // Change to ready again
+               _  = d.innerHTML.assert("(Ready(1),true)")
+               _ <- act(IO.sleep(50.millis))
+               _  = d.innerHTML.assert("(Ready(2),false)")
+             yield ()
+    yield ()
+
+  test("useEffectKeepResultWhenDepsReadyOrChange - call every time deps become ready or change"):
+    val buttonRef = Ref[dom.HTMLButtonElement]
+
+    val comp = ScalaFnComponent[Queue[IO, Int]]: q =>
+      for
+        s <- useState(Pot.pending[MyInt])
+        v <- useEffectKeepResultWhenDepsReadyOrChange(s.value): _ =>
+               IO.sleep(50.millis) >> q.take
+      yield <.button(
+        (v.value, v.isRunning).toString,
+        ^.onClick --> s.modState(_.fold(MyInt(0), _ => MyInt(0), _.inc).ready),
+        ^.onDoubleClick --> s.setState(Pot.error(RuntimeException("error")))
+      ).withRef(buttonRef)
+
+    for
+      q <- buildQueue
+      _ <- withRendered(comp(q)): d =>
+             d.innerHTML.assert("(Pending,false)")
+             for
+               _ <- act(IO.sleep(50.millis))
+               _  = d.innerHTML.assert("(Pending,false)")             // Still pending
+               _ <- act_(Simulate.click(buttonRef.unsafeGet()))       // Change to ready
+               _  = d.innerHTML.assert("(Pending,true)")
+               _ <- act(IO.sleep(50.millis))
+               _  = d.innerHTML.assert("(Ready(1),false)")
+               _ <- act_(Simulate.click(buttonRef.unsafeGet()))       // Increment, don't change value
+               _  = d.innerHTML.assert("(Ready(1),false)")
+               _ <- act_(Simulate.click(buttonRef.unsafeGet()))       // Increment again, should change
+               _  = d.innerHTML.assert("(Ready(1),true)")
+               _ <- act(IO.sleep(50.millis))
+               _  = d.innerHTML.assert("(Ready(2),false)")
+               _ <- act_(Simulate.doubleClick(buttonRef.unsafeGet())) // Change to error
+               _  = d.innerHTML.assert("(Ready(2),false)")            // No change in value
+               _ <- act_(Simulate.click(buttonRef.unsafeGet()))       // Change to ready again
+               _  = d.innerHTML.assert("(Ready(2),true)")
+               _ <- act(IO.sleep(50.millis))
+               _  = d.innerHTML.assert("(Ready(3),false)")
+             yield ()
+    yield ()
